d6411c75f2580ea1bcbd031b6836a69e2aee098e
danielSafonyan <daniel.safonyan@gmail.com>
2022-07-03 20:26:54 +0100

    New feature: create mustangs form csv.

--- MustangCreator.py
+++ MustangCreator.py
@@ -1,7 +1,13 @@
-from LinkExtractor import CarLinkExtractor
+import csv
+import copy
+
 
 class Mustang:
-    all_mustangs = []
+    __all_mustangs = []
+
+    @classmethod
+    def get_mustang_list(cls):
+        return copy.deepcopy(cls.__all_mustangs)
 
     def __init__(self, specification, location, price, mileage, year, img_url):
         self.__specification = specification
@@ -12,19 +18,28 @@ class Mustang:
         self.__img_url = img_url
 
         print('Mustang created.')
-        Mustang.all_mustangs.append(self)
+        Mustang.__all_mustangs.append(self)
 
     def __str__(self):
         return f'This is a Ford Mustang {self.__specification}'
 
+    def __repr__(self):
+        return f'Ford Mustang {self.__specification}'
+
     @classmethod
-    def create_mustang_objects(cls, car_dicts):
-        for dict_ in car_dicts:
-            Mustang(
-                specification=dict_['specification'],
-                location=dict_['location'],
-                price=dict_['price'],
-                mileage=dict_['mileage'],
-                year=dict_['year'],
-                img_url=dict_['img_url'],
-            )
+    def create_mustang_objects_from_csv(cls):
+        with open('mustangs.csv', 'r') as file:
+            csv_reader = csv.DictReader(file)
+            for line in csv_reader:
+                Mustang(
+                    specification=line['specification'],
+                    location=line['location'],
+                    price=line['price'],
+                    mileage=line['mileage'],
+                    year=line['year'],
+                    img_url=line['img_url'],
+                )
+
+
+Mustang.create_mustang_objects_from_csv()
+print(Mustang.get_mustang_list())
--- testing.py
+++ testing.py
@@ -1,30 +1,6 @@
 import csv
 
-
-class CarLinkExtractor:
-    car_dicts = []
-
-    @classmethod
-    def save_to_csv(cls):
-        pass
-
-this_car = {
-    'specification': "GT",
-    'location': "USA",
-    'price': "21",
-}
-
-for i in range(5):
-    CarLinkExtractor.car_dicts.append(this_car)
-
-for i in CarLinkExtractor.car_dicts:
-    print(i)
-
-with open('mustangs.csv', 'w') as file:
-    fieldnames = ['specification', 'location', 'price']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader()
-    for car in CarLinkExtractor.car_dicts:
-        writer.writerow(car)
-
-
+with open('mustangs.csv', 'r') as file:
+    csv_reader = csv.DictReader(file)
+    for line in csv_reader:
+        print(line)
